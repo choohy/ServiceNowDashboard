19a373b2f3d38cf9925d78b2e8f20da94094e983
Choo, Hwan-Yi <Hwan-Yi.Choo@anz.com>
2020-11-12 19:44:46 +1100

    Filter doesn't work but the preview does.

--- src/snc-now-experience-dashboard/snc-now-experience-dashboard.js
+++ src/snc-now-experience-dashboard/snc-now-experience-dashboard.js
@@ -5,17 +5,32 @@ import styles from './styles.scss';
 
 import '../components/now-experience-table';
 import '../components/now-experience-filter';
+import '../components/now-experience-record-preview';
+
 import { columns } from './defaults.js';
 
 createCustomElement('snc-now-experience-dashboard', {
     view: function (state, helpers) {
-        const { dataRows } = state;
+        const {
+            dataRows,
+            recordTitle,
+            recordDetails,
+            items
+        } = state;
+
         const displayColumns = columns.filter((col) => {
             return col.field !== 'sys_id';
         });
 
         return (
             <div className="container">
+                <div className="panel-content">
+                    <now-experience-record-preview
+                        items={items}
+                        recordTitle={recordTitle}
+                        recordDetails={recordDetails}
+                    ></now-experience-record-preview>
+                </div>
                 <div className="table-content">
                     <now-experience-filter></now-experience-filter>
                     <now-experience-table
